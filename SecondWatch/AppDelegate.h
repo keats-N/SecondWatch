1856743488db954a2b06e2946975e16df8ac8b6f
nd <nd@jiangljdemac-mini.local>
2016-07-25 10:04:12 +0800

    Initial Commit

---
+++ SecondWatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SecondWatch
+//
+//  Created by nd on 16/7/25.
+//  Copyright © 2016年 com.nd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SecondWatch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SecondWatch
+//
+//  Created by nd on 16/7/25.
+//  Copyright © 2016年 com.nd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
